51aba3fde4d332e666d52c901f2801b51d8e33ec
Matheus Marrane <matheus.chagas@topaziopartner.com.br>
2020-11-17 23:33:26 -0300

    Update add method from UserService.js in order to add info to storage

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,17 +1,21 @@
-const initialData = {
-  users: [],
-};
+import AsyncStorage from '@react-native-async-storage/async-storage';
 export class UserService {
-  static users = initialData.users;
-
-  static async add(name, phone, age, address, symptoms) {}
+  //Add an user to storage
+  static async add(name, phone, age, address, symptoms) {
+    let usersStorage = await AsyncStorage.getItem('@users');
+    let users = {};
+    //check if @users are already initiated and parse the JSON string
+    if (usersStorage) {
+      users = JSON.parse(usersStorage);
+    }
+    users[name + phone] = {name, phone, age, address, symptoms};
+    AsyncStorage.setItem('@users', JSON.stringify(users));
+  }
 
   static async remove(name, phone) {}
 
   //key is name+phone
   static async update(key, name, phone, age, address, symptoms) {}
 
-  static clean() {
-    UserService.users = initialData;
-  }
+  static get(filter = {}) {}
 }
