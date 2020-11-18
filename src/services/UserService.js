2a9d0efb7a52a87c32e2134758d1d147a87f1767
Matheus Marrane <matheus.chagas@topaziopartner.com.br>
2020-11-18 00:23:43 -0300

    Fix UserService.js to call StorageService methods correctly

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -4,20 +4,23 @@ export class UserService {
   //Add an user to storage
   static async add(name, phone, age, address, symptoms) {
     //TODO alert user if name and phone already in storage
-    let usersStorage = await StorageService.getItem('@users');
+    let usersStorage = await StorageService.get('@users');
     let users = {};
     //check if @users are already initiated and parse the JSON string
     if (usersStorage) {
       users = JSON.parse(usersStorage);
     }
     users[name + phone] = {name, phone, age, address, symptoms};
-    StorageService.setItem('@users', JSON.stringify(users));
+    await StorageService.set('@users', JSON.stringify(users));
   }
 
+  //TODO remove user
   static async remove(name, phone) {}
 
+  //TODO updateUser
   //key is name+phone
   static async update(key, name, phone, age, address, symptoms) {}
 
+  //TODO handle filters
   static get(filter = {}) {}
 }
