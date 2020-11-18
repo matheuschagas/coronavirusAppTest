8504641d69d082108caed65b4cb7f99b276d5a3d
Matheus Marrane <matheus.chagas@topaziopartner.com.br>
2020-11-18 09:55:53 -0300

    Setup jest

---
+++ jest/setup.js
@@ -0,0 +1,41 @@
+import * as ReactNative from 'react-native';
+import 'react-native-gesture-handler/jestSetup';
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+
+  return Reanimated;
+});
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+
+jest.doMock('react-native', () => {
+  // Extend ReactNative
+  return Object.setPrototypeOf(
+    {
+      Platform: {
+        ...ReactNative.Platform,
+        OS: 'android',
+        Version: 123,
+        isTesting: true,
+        select: (objs) => objs.ios,
+      },
+      StyleSheet: {
+        ...ReactNative.StyleSheet,
+      },
+      NativeModules: {
+        ...ReactNative.NativeModules,
+        PlatformLocalStorage: {},
+        RNC_AsyncSQLiteDBStorageRNC_AsyncSQLiteDBStorage: {},
+        RNCAsyncStorage: {},
+        RNVectorIconsManager: {},
+      },
+    },
+    ReactNative,
+  );
+});
